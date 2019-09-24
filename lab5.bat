f343cd247501cd926c7fb28c06f9fc8e564aac58
stasenash <staslisenash@gmail.com>
2019-09-24 20:55:19 +0500

    Add files via upload

--- lab5.bat
+++ lab5.bat
@@ -1,2 +1,20 @@
 @echo off
-FOR %%i in (%1\*.*) do echo %%~ti
+chcp 1251 >nul
+setlocal enabledelayedexpansion
+md "%1\sorted"
+for /f "tokens=*" %%i in ('dir %1\*.* /b') do @(
+    set ext=%%~xi
+	if "!ext:~1!"=="~1" if not exist "%1\sorted\noext" md "%1\sorted\noext"
+    if not exist "%1\sorted\!ext:~1!" if not "!ext:~1!"=="~1" md "%1\sorted\!ext:~1!"
+)
+for /f "tokens=*" %%i in ('dir %1\*.* /b') do (
+	set ext=%%~xi
+    if not "!ext:~1!"=="~1" if not exist "%1\sorted\!ext:~1!\%%i" copy "%1\%%i" "%1\sorted\!ext:~1!"
+	if "!ext!"=="" copy "%1\%%i" "%1\sorted\noext"
+)
+for /f "tokens=*" %%i in ('dir %1\*.* /b /a-d') do (
+	set ext=%%~xi
+    if not "!ext:~1!"=="~1" echo %%~nxi - %%~ti >> "%1\sorted\!ext:~1!\report.txt"
+	if "!ext!"=="" echo %%~nxi - %%~ti >> "%1\sorted\noext\report.txt"
+)
+
