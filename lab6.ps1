7cb5c68d6da5a6916461cbf62cabd16788215d9f
stasenash <staslisenash@gmail.com>
2019-09-29 12:52:16 +0500

    Add files via upload

---
+++ lab6.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host("Enter the first num")
+[int]$num1 =  Read-Host
+Write-Host("Enter the second num")
+[int]$num2 =  Read-Host
+Write-Host("Enter the operation")
+$operation = Read-Host
+
+if ($operation -eq "+")
+{
+	Write-Host("Your result is: "+($num1+$num2))
+}
+if ($operation -eq "-")
+{
+	Write-Host("Your result is: "+($num1-$num2))
+}
+if ($operation -eq "*")
+{
+	Write-Host("Your result is: "+($num1*$num2))
+}
+if ($operation -eq "/")
+{
+	if (-not($num2 -eq 0))
+    {
+		Write-Host("Your result is: "+($num1/$num2))
+    }
+	else
+    {
+		Write-Host("Cannot be divided by zero")
+    }
+}
